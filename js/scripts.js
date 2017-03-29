08afbe9df99c0ce1ac7c974f8ec0ebd4f3f710c9
Grady Shelton and Jennifer Kinsey <grady@>
2017-03-29 12:08:17 -0700

    add and check third condition to see if first word begins with consonant. if so, we will add consonant and ay to end of string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,7 @@
 // business logic
 
 var vowels = ["a", "e", "i", "o", "u"];
+var consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
 
 // this check first word of full string, for a non-alpha, returns an alert if it is
 var isNonAlpha = function(input){
@@ -12,10 +13,19 @@ var isNonAlpha = function(input){
 }
 
 //this checks for a index(0) is a vowel, if so, it will append "ay" to the end of the input
-var isVowelFirst = function(input){
+
+//then this checks to see if index(0) is a consonant, if so, it will append the consonant and "ay" to the end
+var isVowelOrConsonantFirst = function(input){
   for(var i = 0; i < vowels.length; i++){
     if(input.charAt(0) === vowels[i]){
       input = input + "ay";
+    }
+  }
+  for(var i = 0; i < consonants.length; i++){
+    if(input.charAt(0) === consonants[i]){
+      // take the first and put it at the end
+      input = input.substring(1, input.length) + (input.charAt(0) + "ay");
+      // then add ay
     }
   }
   return input;
@@ -29,7 +39,8 @@ $(function() {
     var userInput = $("#userInput").val().toLowerCase();
     var alphaResult = isNonAlpha(userInput);
 
-    var result1 = isVowelFirst(userInput);
+    var result1 = isVowelOrConsonantFirst (userInput);
+    // result1 = isConsonant(userInput);
 
     $(".result").show();
 
@@ -38,6 +49,7 @@ $(function() {
     }
 
     $("#pigResult").append(result1);
+
 
     this.reset();
 
