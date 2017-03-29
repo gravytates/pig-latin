3bc8a24facb3b788a509ce2ab181c6ac171d5eb9
Grady Shelton and Jennifer Kinsey <jennifer@>
2017-03-29 11:18:17 -0700

    add and check second condition to see if first word begins with vowel. if so, we will add ay to the end of the string.

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,45 @@
 // business logic
+
+var vowels = ["a", "e", "i", "o", "u"];
+
+// this check first word of full string, for a non-alpha, returns an alert if it is
 var isNonAlpha = function(input){
   if (input.charAt(0).match(/[a-z]/i)){
-    alert("there are letters");
+    return true;
   } else {
-    alert("this begins with a non-alpha character");
+    return false;
   }
 }
 
+//this checks for a index(0) is a vowel, if so, it will append "ay" to the end of the input
+var isVowelFirst = function(input){
+  for(var i = 0; i < vowels.length; i++){
+    if(input.charAt(0) === vowels[i]){
+      input = input + "ay";
+    }
+  }
+  return input;
+}
 
 // ui logic
 $(function() {
 
   $("form").submit(function(event){
     event.preventDefault();
-    var userInput = $("#userInput").val();
-    isNonAlpha(userInput);
+    var userInput = $("#userInput").val().toLowerCase();
+    var alphaResult = isNonAlpha(userInput);
+
+    var result1 = isVowelFirst(userInput);
 
     $(".result").show();
 
+    if (!alphaResult) {
+      $(".alerts").append("<li>Please have your input begin with a letter!</li>");
+    }
 
+    $("#pigResult").append(result1);
 
-
+    this.reset();
 
 
   }); //this closes form submit
